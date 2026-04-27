767781b3717fca0990dfafe8bb9651f99d5461bd
M Agni A Fadhly S <fadhlysaepudin@gmail.com>
2026-04-27 09:17:39 +0700

    fix: space

--- soal1.c
+++ soal1.c
@@ -87,12 +87,12 @@ int main() {
         //int temp = (count+1) / 2;
         //printf("%d\n", temp);
         int median = arr[(count+1) / 2 -1];
-        printf("MEDIAN  %d", median);
+        printf("MEDIAN %d\n", median);
     } else if (count%2==0) {
         float a = (arr[count/2-1]+arr[count/2]);
         float b = 2.00;
         float median = a/b;
-        printf("MEDIAN  %.2f", median);
+        printf("MEDIAN %.2f\n", median);
     } else {
         printf("error\n");    
     }
