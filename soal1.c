b3a14e6a426a5564de8e38cd6784eacef9be69ed
M Agni A Fadhly S <fadhlysaepudin@gmail.com>
2026-04-27 09:12:57 +0700

    fix: float median

--- soal1.c
+++ soal1.c
@@ -89,7 +89,9 @@ int main() {
         int median = arr[(count+1) / 2 -1];
         printf("MEDIAN  %d\n", median);
     } else if (count%2==0) {
-        float median = (arr[count/2]+arr[count/2 + 1 -1])/2;
+        float a = (arr[count/2-1]+arr[count/2]);
+        float b = 2.00;
+        float median = a/b;
         printf("MEDIAN  %.2f\n", median);
     } else {
         printf("error\n");    
